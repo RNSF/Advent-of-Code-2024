8cd79e610250b4903a65f91efe4317a4f27aee32
rnsfdev <ahrenrsf@gmail.com>
2024-12-06 19:41:28 -0800

    day 2 setup

---
+++ Day2/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+
+int part_one(string input_path) {
+	ifstream infile(input_path);
+}
+
+
+int main() {
+	cout << part_one("input.txt") << "\n";
+}
